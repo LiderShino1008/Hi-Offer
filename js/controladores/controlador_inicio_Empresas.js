22a3f36edae9cb2757d3c9336f3c33dc32bef8c0
GabrielaCabrera1 <60476428+GabrielaCabrera1@users.noreply.github.com>
2020-04-19 21:54:52 -0800

    Desarrollo del perfil empresarial

--- js/controladores/controlador_inicio_Empresas.js
+++ js/controladores/controlador_inicio_Empresas.js
@@ -60,3 +60,38 @@ var myBarChart = new Chart(ctxB, {
     }
   }
 });
+
+function iniciar() {
+  document.getElementById('dashboard').classList.remove('d-none');
+  document.getElementById('item-dashboard').classList.add('seleccionar');
+  document.getElementById('item-perfil').classList.remove('seleccionar');
+  document.getElementById('perfil-empresarial').classList.add('d-none');
+}iniciar();
+
+//Controlador 
+function dashboard(){
+  document.getElementById('dashboard').classList.remove('d-none');
+  document.getElementById('item-dashboard').classList.add('seleccionar');
+  document.getElementById('item-perfil').classList.remove('seleccionar');
+  document.getElementById('perfil-empresarial').classList.add('d-none')
+}
+
+function perfil(){
+  document.getElementById('dashboard').classList.add('d-none');
+  document.getElementById('item-dashboard').classList.remove('seleccionar');
+  document.getElementById('item-perfil').classList.add('seleccionar');
+  document.getElementById('perfil-empresarial').classList.remove('d-none');
+}
+
+/* FUNCION PARA GENERAR MAPA*/
+function generarMapa(){
+  let latitud;
+  let longitud;
+  var coord={latitud, longitud}
+  var map=new google.map.Map(documen.getElementById(''),{
+    zoom: 10,
+    center:coord
+
+  }) //donde vamos a ubicar el mapa
+
+}
